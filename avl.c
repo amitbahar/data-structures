49f83963ad042608fb94eff5babc825c53b6f6e3
Amit <sr.amit.bachar@gmail.com>
2022-11-28 12:32:35 +0200

    implemented simple insert with no rotations

--- avl.c
+++ avl.c
@@ -6,14 +6,20 @@ typedef struct Node {
 	struct Node *parent;
 	int height;
 	int data;
+	char virtual; //0 == no, 1 == yes
 } Node;
 
 typedef struct Tree {
 	struct Node *head;
+	struct Node *virtual;
 } Tree;
 
+
 Tree * makeTree() {
-	Tree tree = {NULL};
+	Node *tmp = (Node *)malloc(sizeof(Node));
+	*tmp = (Node){NULL,NULL,NULL,-1,0,1};
+	Tree *tree = (Tree *)malloc(sizeof(Tree));
+	*tree = (Tree){NULL,tmp};
 	return tree;
 }
 
@@ -35,7 +41,36 @@ void _destroyrec(Node *head) {
 }
 
 void insert(Tree *tree, int data) {
+	Node *new = (Node *)malloc(sizeof(Node));
+	new = (Node){tree->virtual,tree->virtual,NULL,0,data,0};
+	Node *tmp = tree->head;
+	Node *tmpparent;
 	
+	if (tmp == NULL) {
+		tree->head = new;
+		return;
+	} else {
+		while (tmp->virtual != 1) {
+			tmpparent = tmp;
+			if (tmp->data <= data) {
+				tmp = tmp->left;
+			} else {
+				tmp = tmp->right;
+			}
+		}
+		
+		if (tmpparent->left = tmp) {
+			tmpparent->left = new;
+		} else {
+			tmpparent->right = new;
+		}
+		new->parent = tmpparent;
+		
+		while (tmpparent != NULL) {
+			tmpparent->height = tmpparent->left->height-tmpparent->right->height;
+			tmpparent = tmpparent->parent;
+		}
+	}
 }
 
 
