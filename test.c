03661ca2a73b632b81d8d8e893bcf99ceb51d2db
Amit <sr.amit.bachar@gmail.com>
2022-12-13 19:59:56 +0200

    simple insertion and print test

--- test.c
+++ test.c
@@ -1,11 +1,13 @@
 #include "avl.h"
+#include <stdio.h>
 
 int main() {
 	Tree *tree = makeTree();
 	insert(tree,1);
 	insert(tree,2);
 	insert(tree,3);
-	
+	printree(tree);
+	destroy(tree);
 	return 0;
 }
 
